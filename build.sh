c0e7e157183508965c3ed198a451eeb52d3a389e
HinohArata <mmgcntk@gmail.com>
2026-08-27 03:22:04 +0700

    feat(patches): add custom MOTD patch for AuraStudio
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- build.sh
+++ build.sh
@@ -51,6 +51,9 @@ declare -a PATCHES=(
   "libuv-force-cmake-build.patch"
   "coreutils-depend-on-libacl.patch"
   "subversion-missing-apr-includes.patch"
+
+  # Termux motd
+  "termux-tools-motd.patch"
 )
 
 # Optional build-fix patches (applied with --extras flag)
