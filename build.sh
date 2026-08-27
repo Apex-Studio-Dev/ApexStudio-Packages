e49b66729a05312cdb1989fac547ab2c347705f2
HinohArata <mmgcntk@gmail.com>
2026-08-27 15:38:15 +0700

    chore: Add command-not-found packages to bootstrap also patch it
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- build.sh
+++ build.sh
@@ -51,6 +51,7 @@ declare -a PATCHES=(
   "libuv-force-cmake-build.patch"
   "coreutils-depend-on-libacl.patch"
   "subversion-missing-apr-includes.patch"
+  "generate-db-fix-url.patch"
 
   # Termux motd
   "termux-tools-motd.patch"
--- packages.sh
+++ packages.sh
@@ -25,6 +25,7 @@ declare -a AURASTUDIO_PACKAGES__BOOTSTRAP=(
   "tar"
 
   # Termux essentials
+  "command-not-found"
   "termux-core"
   "termux-exec"
   "termux-keyring"
